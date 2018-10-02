0a028a4489b0683f004554f83b20d845a4b8a507
Nikos Giachoudis <elpaxoudis@gmail.com>
2018-10-02 23:59:43 +0300

    removing type declaration from Product.py

--- modules/Product.py
+++ modules/Product.py
@@ -1,10 +1,11 @@
 class Product:
-    def __init__(self, name, price:float):
+    def __init__(self, name, price):
         self.name = name
         self.price = float(price)
-    
+
+
 if __name__ == "__main__":
-    
-    beer = Product("Beer", 2)
+
+    beer = Product("Beer", 5.5)
     print(beer.name, " ", beer.price)
     print(type(beer.price))
