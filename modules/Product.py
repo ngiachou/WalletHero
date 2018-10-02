197b3dafb1529401cc6a95c1af1eaa8b3aba9054
Nikos Giachoudis <elpaxoudis@gmail.com>
2018-10-03 00:11:29 +0300

    adding properties for name and price

--- modules/Product.py
+++ modules/Product.py
@@ -1,11 +1,20 @@
 class Product:
     def __init__(self, name, price):
-        self.name = name
-        self.price = float(price)
+        self._name = name
+        self._price = float(price)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def price(self):
+        return self._price
 
 
 if __name__ == "__main__":
 
     beer = Product("Beer", 5.5)
-    print(beer.name, " ", beer.price)
-    print(type(beer.price))
+    assert(beer.name == "Beer")
+    assert(beer.price == 5.5)
+    print(beer)
