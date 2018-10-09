8d7e1c17045f4725f204466e50241aaf1439fc24
pagidas <pagakrivos@gmail.com>
2018-10-09 18:08:57 +0300

    implementing the Transaction class

--- modules/Transaction.py
+++ modules/Transaction.py
@@ -6,19 +6,30 @@ import datetime
 class Transaction:
 	def __init__(self, product_list, enviroment_characteristic, place, date):
 
-		self.product_list = [Product(object)] #a list which takes products in type of Product
+		self.product_list = product_list
 		self.enviroment_characteristic = enviroment_characteristic
 		self.place = place
-		self.date = date(a_date)
+		self.date = date
 
-	print("\n---constructor begins---\n") #testing purposes
+	def __repr__(self):
+		return "({}, {}, {}, {})".format(self.date, self.enviroment_characteristic, 
+			self.place, self.product_list)
 
-if __name__ == "__main__": #testing purposes
-	print("------hello, main-------\n") #testing purposes
+
+if __name__ == "__main__":
+
+	a_date = datetime.date(2018, 10, 2) #creating a date
 
 	beer = Product("Beer", 4.50)
 	pizza = Product("Pizza", 7.00)
 
-	a_product_list = [beer, pizza]
+	a_product_list = [beer, pizza] #inserting two products, created above
 
-	a_transaction = Transaction(a_product_list, "friends", "in", "2018/10/02")
+	a_transaction = Transaction(a_product_list, "friends", "in", a_date) #initializing a Transaction instance
+
+
+	assert(a_transaction.enviroment_characteristic == "friends")
+	assert(a_transaction.place == "in")
+	assert(str(a_transaction.date) == "2018-10-02")
+	assert(repr(a_transaction.product_list) == "[(Beer, 4.5), (Pizza, 7.0)]")
+	print(a_transaction)
