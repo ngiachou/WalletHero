035d00d3da8daf12d6de8abf89340047871ce97f
kpaxiotis <kostaxiotis@hotmail.com>
2018-10-14 00:03:59 +0300

    Removing history_of_transactions attribute and setting balance's default value
    Testing code:
    
    f __name__ == "__main__":
    
        beer = Product("Beer", 3.5)                 #Creating a transaction
        pizza = Product("Pizza", 7)
        a_date = datetime.date(2018, 10, 2)
        a_product_list = [beer, pizza]
        a_transaction = Transaction(a_product_list, "friends", "in", a_date)
    
        coffee = Product("Coffee", 3)               #Creating a second transaction
        b_date = datetime.date(2018, 10, 1)
        b_product_list = coffee
        b_transaction = Transaction(b_product_list, "significant other", "out", b_date)
    
        a_history_of_transactions = []
    
        PersonalBank.add_transaction(a_history_of_transactions, a_transaction)
        PersonalBank.add_transaction(a_history_of_transactions, b_transaction)
    
        a_personal_bank = PersonalBank()
        b_personal_bank = PersonalBank(400)
    
        PersonalBank.show_transaction_history(a_history_of_transactions)
        print(a_personal_bank)                      #testing repr function
    
        assert(b_personal_bank.balance == 400)      #testing if balance can take a value manually
    
    Response:
    (2018-10-01, significant other, out, (Coffee, 3.0))
    (2018-10-02, friends, in, [(Beer, 3.5), (Pizza, 7.0)])
    350

--- modules/PersonalBank.py
+++ modules/PersonalBank.py
@@ -14,10 +14,12 @@ class PersonalBank:
     """
 
     
-    def __init__(self, balance, history_of_transactions):      
-    
-        self.balance = balance
-        self.history_of_transactions = history_of_transactions
+    def __init__(self, balance = None):      
+        
+        if balance == None:
+            self.balance = 350
+        else:
+            self.balance = balance
 
     def add_transaction(history_of_transactions, transaction):
 
@@ -32,30 +34,33 @@ class PersonalBank:
 
     def __repr__(self):
         
-        return "({}, {})".format(self.balance, self.history_of_transactions)
+        return "{}".format(self.balance)
         
         
         
 if __name__ == "__main__":
 
-    beer = Product("Beer", 3.5)     #Creating a transaction
+    beer = Product("Beer", 3.5)                 #Creating a transaction
     pizza = Product("Pizza", 7)
     a_date = datetime.date(2018, 10, 2)
     a_product_list = [beer, pizza]
     a_transaction = Transaction(a_product_list, "friends", "in", a_date)
 
-    coffee = Product("Coffee", 3)   #Creating a second transaction
+    coffee = Product("Coffee", 3)               #Creating a second transaction
     b_date = datetime.date(2018, 10, 1)
     b_product_list = coffee
     b_transaction = Transaction(b_product_list, "significant other", "out", b_date)
     
     a_history_of_transactions = []
     
-    my_personal_bank = PersonalBank(350, a_history_of_transactions)
-    
     PersonalBank.add_transaction(a_history_of_transactions, a_transaction)
     PersonalBank.add_transaction(a_history_of_transactions, b_transaction) 
     
+    a_personal_bank = PersonalBank()
+    b_personal_bank = PersonalBank(400)
     
-
     PersonalBank.show_transaction_history(a_history_of_transactions)
+    print(a_personal_bank)                      #testing repr function
+
+    assert(b_personal_bank.balance == 400)      #testing if balance can take a value manually
+    
