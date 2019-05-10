97667e3dbb270706c489a879a59e799b478b5ba7
pagidas <pagakrivos@gmail.com>
2019-05-10 22:43:41 +0300

    Adds CORS headers for all domains

--- wh-django/wallet_hero/settings.py
+++ wh-django/wallet_hero/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # my apps
     'rest_framework',
+    'corsheaders'
     'users',
 ]
 
@@ -52,7 +53,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # corsheaders-related
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
+
+# Enable CORS for all domains
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'wallet_hero.urls'
 
